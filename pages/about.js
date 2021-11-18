6d16c3ee7a14e171738c874d7bdc52d4932b8f00
rnagella <nagella.ranjith@gmail.com>
2021-11-17 20:29:10 -0800

    Add about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+// pages/about.js
+
+// This page can can be pre-rendered without
+// external data: It will be pre-rendered
+// into a HTML file at build time.
+export default function About() {
+  return <div>
+    <h1>About Us</h1>
+    {/* ... */}
+  </div>
+}
